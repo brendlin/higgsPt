c703c592a237480d6787a9699f999d19e28c988f
Kurt Brendlinger <kurt.brendlinger@cern.ch>
2021-03-16 15:07:05 +0100

    Add simple Rivet running instructions

---
+++ rivet/README.md
@@ -0,0 +1,34 @@
+
+How to run Rivet with Athena on output EVNT.root files
+============
+
+First, make sure you have successfully produced an EVNT.root file
+using Madgraph interfaced to Athena. Then proceed with the following steps.
+
+Setup
+---------
+asetup 21.6.58,AthGeneration
+source setupRivet.sh # This does not look like it should work, but it does
+ln -s ../madgraphsetup/Rivet/HiggsDiphotonFiducialCrossSectionAnalysis2020.cc .
+
+Running
+---------
+
+This takes in an EVNT.root file, and spits out a Yoda file. One question you may ask is,
+"Is there a way to automatically create a root file instead of a Yoda file during the athena
+running" ? The answer is "no" -- it used to be possible, but due to an update in Yoda histogram
+naming that was not propagated to `Generators/Rivet_i/src/Rivet_i.cxx`, the root histograms will
+simply not show up, without throwing any warning. See below for how to convert a yoda file to a
+root histogram file.
+
+```
+rivet-build RivetAnalysis.so HiggsDiphotonFiducialCrossSectionAnalysis2020.cc
+athena RivetAnalysis.py -c 'directory="../MG_athena/run_generateFromGridpack"; filename="EVNT.root"'
+```
+
+Converting output to a Root File
+----------
+
+```bash
+$AthGeneration_DIR/src/Generators/Rivet_i/share/example/convert2root myanalysis.yoda
+```
---
+++ rivet/RivetAnalysis.py
@@ -0,0 +1,46 @@
+theApp.EvtMax = -1
+
+import os,sys
+import glob
+
+import AthenaPoolCnvSvc.ReadAthenaPool
+from AthenaCommon.AppMgr import ServiceMgr as svcMgr
+
+#svcMgr.EventSelector.InputCollections = ["/afs/cern.ch/user/s/sheim/public/ForRivetPackage/EVNT.05895633._000030.pool.root.1"]
+#svcMgr.EventSelector.InputCollections = ["/afs/cern.ch/work/s/sheim/HZZ/CrossSections/Rivet/evgenfile/mc15_13TeV/EVNT.05550958._000201.pool.root.1"]
+
+# See if a filename was specified, otherwise set to EVNT.root
+try :
+    tmp = filename
+except NameError :
+    filename = 'EVNT.root'
+
+try :
+    print 'Finding all files named %s in directory %s'%(filename,directory)
+except NameError :
+    print 'Usage: python RivetAnalysis.py',
+    print '-c \'directory = "../path/to/location/of/basedir"; filename "EVNT.root"\''
+    print '(e.g. the script will look for all files named e.g. EVNT.root inside of basedir)'
+    print 'Exiting -- please try the usage described above.'
+    sys.exit()
+
+list_of_files = list(os.path.abspath(i) for i in glob.glob('%s/*%s'%(directory,filename)))
+svcMgr.EventSelector.InputCollections = list_of_files
+print svcMgr.EventSelector.InputCollections
+
+from AthenaCommon.AlgSequence import AlgSequence
+job = AlgSequence()
+
+from Rivet_i.Rivet_iConf import Rivet_i
+rivet = Rivet_i("Rivet")
+rivet.AnalysisPath = os.environ['PWD']
+rivet.Analyses += [ 'HiggsDiphotonDifferentialCrossSection2020' ]
+rivet.RunName = 'Rivet'
+
+rivet.HistoFile = 'Rivet.yoda'
+rivet.DoRootHistos = False
+rivet.CrossSection = 1.0
+job += rivet
+
+import AthenaCommon.AlgSequence as acas
+acas.dumpMasterSequence()
