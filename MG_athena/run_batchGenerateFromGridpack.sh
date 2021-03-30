55413cc542142990c8211619321d397c2f272725
Kurt Brendlinger <kurt.brendlinger@cern.ch>
2021-03-30 21:29:23 +0200

    First working example of batch running

---
+++ MG_athena/run_batchGenerateFromGridpack.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# asetup AthGeneration,21.6.57,here
+THEDIR=run_batchFromGridpack
+THERUNNUMBER=999999
+THEJO=mc.aMCPy8EG_ppToHj_SMEFTatNLO_GridPack.py
+
+TMP=${THEJO/mc\./mc_13TeV\.}
+THEGRIDPACK=${TMP/\.py/\.GRID\.tar\.gz}
+THEGRIDPACKDIR=${THEDIR/batchFrom/make}
+
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+# Test to make sure that the gridpack exists!
+echo  -e ${GREEN} Checking for Gridpack $THEGRIDPACKDIR/$THEGRIDPACK ${NC};
+if [ ! -f $THEGRIDPACKDIR/$THEGRIDPACK ]; then
+    echo  -e ${GREEN} Could not find Gridpack $THEGRIDPACKDIR/$THEGRIDPACK. Stopping. ${NC};
+else
+    # Make the directory, copy submit and run files there.
+    mkdir -p $THEDIR
+    cp submit $THEDIR/.
+
+    cp gridrun $THEDIR/.
+    cp submit $THEDIR/.
+
+    cd $THEDIR
+    echo condor_submit submit THEDIR=$THEDIR THEJO=$THEJO
+    condor_submit submit THEDIR=$THEDIR THEJO=$THEJO 
+    cd -
+    echo -e ${GREEN} Submitted. ${NC};
+fi;
+
+echo -e ${GREEN} Done. ${NC};
